46e65c079c17418e0582618e4b4e70280a81c47a
dungne1104 <100553554+duphlot@users.noreply.github.com>
2022-11-26 07:59:15 +0700

    Có nỗi niềm chất chứa trong anh cao thêm

---
+++ lgh/tiki/test-DESKTOP-R9AAUQL.py
@@ -0,0 +1,17 @@
+try:
+    print(3)
+except:
+    print(4)
+
+
+try:
+    c = input()
+    print(c*c)
+except:
+    print(4)
+
+
+try:
+    print(3)
+except:
+    print(4)
---
+++ lgh/tiki/test.py
@@ -0,0 +1,17 @@
+try:
+    print(3)
+except:
+    print(4)
+
+
+try:
+    c = input()
+    print(c*C)
+except:
+    print(4)
+
+
+try:
+    print(3)
+except:
+    print(4)
