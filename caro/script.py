3c9c8fc51de77e00ceb139d7d06cae6aaf72e5d2
dungne1104 <100553554+duphlot@users.noreply.github.com>
2022-12-29 13:51:30 +0700

    simplee

--- caro/script.py
+++ caro/script.py
@@ -1,16 +1,55 @@
 from tic_tac_toe import *
+from copy import deepcopy
 
-my_board = [
-	["1", "2", "X"],
+def game_is_over(board):
+  return has_won(board, "X") or has_won(board, "O") or len(available_moves(board)) == 0
+
+def evaluate_board(board):
+  if has_won(board, "X"):
+    return 1
+  elif has_won(board, "O"):
+    return -1
+  else:
+    return 0
+
+new_game = [
+	["1", "2", "3"],
 	["4", "5", "6"],
 	["7", "8", "9"]
 ]
 
-select_space(my_board, 5, "X")
-select_space(my_board, 4, "X")
-select_space(my_board, 6, "X")
-available_moves(my_board)
-print_board(my_board)
-has_won(my_board,"X")
-#print_board(my_board)
+x_winning = [
+	["X", "2", "O"],
+	["4", "O", "6"],
+	["7", "8", "X"]
+]
 
+o_winning = [
+	["X", "X", "O"],
+	["4", "X", "6"],
+	["7", "O", "O"]
+]
+
+def minimax(input_board, is_maximizing):
+  # Base case - the game is over, so we return the value of the board
+  if game_is_over(input_board):
+    return evaluate_board(input_board)
+  if is_maximizing == True:
+    best_value = -float("Inf")
+    symbol = "X"
+  else:
+    best_value = float("Inf")
+    symbol = "O"
+  for move in available_moves(input_board):
+    new_board = deepcopy(input_board)
+    select_space(new_board, move, symbol)
+    hypothetical_value = minimax(new_board,not is_maximizing)
+    if (is_maximizing):
+      best_value = max(hypothetical_value,best_value)
+    else:
+      best_value = min(hypothetical_value,best_value)
+  return best_value
+
+print(minimax(x_winning,True))
+print(minimax(o_winning,True))
+  
