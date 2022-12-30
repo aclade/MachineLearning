24decefccb0c1ed3137e7234ae888cf3f53db9c7
dungne1104 <100553554+duphlot@users.noreply.github.com>
2022-12-30 22:33:17 +0700

    c..cung de thunn

--- caro/script.py
+++ caro/script.py
@@ -1,55 +1,14 @@
 from tic_tac_toe import *
-from copy import deepcopy
 
-def game_is_over(board):
-  return has_won(board, "X") or has_won(board, "O") or len(available_moves(board)) == 0
-
-def evaluate_board(board):
-  if has_won(board, "X"):
-    return 1
-  elif has_won(board, "O"):
-    return -1
-  else:
-    return 0
-
-new_game = [
+my_board = [
 	["1", "2", "3"],
 	["4", "5", "6"],
 	["7", "8", "9"]
 ]
 
-x_winning = [
-	["X", "2", "O"],
-	["4", "O", "6"],
-	["7", "8", "X"]
-]
-
-o_winning = [
-	["X", "X", "O"],
-	["4", "X", "6"],
-	["7", "O", "O"]
-]
-
-def minimax(input_board, is_maximizing):
-  # Base case - the game is over, so we return the value of the board
-  if game_is_over(input_board):
-    return evaluate_board(input_board)
-  if is_maximizing == True:
-    best_value = -float("Inf")
-    symbol = "X"
-  else:
-    best_value = float("Inf")
-    symbol = "O"
-  for move in available_moves(input_board):
-    new_board = deepcopy(input_board)
-    select_space(new_board, move, symbol)
-    hypothetical_value = minimax(new_board,not is_maximizing)
-    if (is_maximizing):
-      best_value = max(hypothetical_value,best_value)
-    else:
-      best_value = min(hypothetical_value,best_value)
-  return best_value
-
-print(minimax(x_winning,True))
-print(minimax(o_winning,True))
-  
+#while not game_is_over(my_board):
+#  select_space(my_board, minimax(my_board, True)[1], "X")
+#  print_board(my_board)
+#  if not game_is_over(my_board):
+#    select_space(my_board, minimax(my_board, False)[1], "O")
+#    print_board(my_board)  
--- caro/tic_tac_toe.py
+++ caro/tic_tac_toe.py
@@ -42,3 +42,57 @@ def has_won(board, player):
     if board[0][2] == player and board[1][1] == player and board[2][0] == player:
         return True
     return False
+  
+  
+from tic_tac_toe import *
+from copy import deepcopy
+
+def game_is_over(board):
+  return has_won(board, "X") or has_won(board, "O") or len(available_moves(board)) == 0
+
+def evaluate_board(board):
+  if has_won(board, "X"):
+    return 1
+  elif has_won(board, "O"):
+    return -1
+  else:
+    return 0
+
+def minimax(input_board, is_maximizing):
+  # Base case - the game is over, so we return the value of the board
+  if game_is_over(input_board):
+    return [evaluate_board(input_board), ""]
+  # The maximizing player
+  if is_maximizing:
+    # The best value starts at the lowest possible value
+    best_value = -float("Inf")
+    best_move = ""
+    # Loop through all the available moves
+    for move in available_moves(input_board):
+      # Make a copy of the board and apply the move to it
+      new_board = deepcopy(input_board)
+      select_space(new_board, move, "X")
+      # Recursively find your opponent's best move
+      hypothetical_value = minimax(new_board, False)[0]
+      # Update best value if you found a better hypothetical value
+      if hypothetical_value > best_value:
+        best_value = hypothetical_value
+        best_move = move
+    return [best_value, best_move]
+  # The minimizing player
+  else:
+    # The best value starts at the highest possible value
+    best_value = float("Inf")
+    best_move = ""
+    # Testing all potential moves
+    for move in available_moves(input_board):
+      # Copying the board and making the move
+      new_board = deepcopy(input_board)
+      select_space(new_board, move, "O")
+      # Passing the new board back to the maximizing player
+      hypothetical_value = minimax(new_board, True)[0]
+      # Keeping track of the best value seen so far
+      if hypothetical_value < best_value:
+        best_value = hypothetical_value
+        best_move = move
+    return [best_value, best_move]
